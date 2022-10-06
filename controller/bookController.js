311925785802901239195c87b9da6dfd48f90c9e
Phat Panhareach <panhareach.phat@student.cadt.edu.kh>
2022-10-06 21:52:00 +0700

    finished api

--- controller/bookController.js
+++ controller/bookController.js
@@ -15,7 +15,7 @@ const getBookById = async (req, res) => {
     try {
         const book = await Book.findById(id)
         if(book == null) res.status(404).json({message: 'Cannot GET the book by id'})
-        res.status(200).json(book)
+        else res.status(200).json(book)
     } catch (error) {
         res.status(404).json({message: 'Cannot GET the book by id'})
         console.log(error.message)
@@ -38,7 +38,7 @@ const deleteSingleBook = async (req, res) => {
     try {
         const book = await Book.findByIdAndDelete(id)
         if(book == null) res.status(404).json({message: 'Cannot DELETE the book by id'})
-        res.status(200).json(book)
+        else res.status(200).json(book)
     } catch (error) {
         res.status(404).json({message: 'cannot DELETE the book by id'})
         console.log(error.message)
@@ -50,7 +50,7 @@ const updateSingleBook = async (req, res) => {
     try {
         const book = await Book.findByIdAndUpdate(id, req.body)
         if(book == null) res.status(404).json({message: 'Cannot PATCH the book by id'})
-        res.status(200).json(book)
+        else res.status(200).json(book)
     } catch (error) {
         res.status(404).json({message: 'Cannot PATCH the book by id'})
         console.log(error.message)
